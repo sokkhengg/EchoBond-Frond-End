00b00a8bdd28161bf97b681717e536950e89073c
Sokheng <jiangshuxing009@gmail.com>
2022-02-08 14:28:38 -0800

    fixing error company data

--- src/components/Companies/Company.js
+++ src/components/Companies/Company.js
@@ -1,47 +1,20 @@
-import { useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
-import Search from './Search';
+import React from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
-function Company({companies, viewCampgrounds, viewToDos}) {
-    const [searchField, setSearchField] = useState("");
-
-    const filteredCompanies = companies.filter(
-        (company) => {
-        return (
-        company
-        .name
-        .toLowerCase()
-        .includes(searchField.toLowerCase())
-        )}
-    )
-    
-    function handleChange(e) {
-        setSearchField(e.target.value)
-        console.log(e.target.value)
-    }
-    
-    return (
-        <div className="page-body">
-            <div className="search">
-                <FaSearch />
-                <input
-                    type="search"
-                    placeholder=" Search for a National Park"
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="results">
-                <Search filteredCompanies={filteredCompanies} viewCampgrounds={viewCampgrounds} viewToDos={viewToDos}/>    
-            </div>
-            <hr />
-
-        {/* <div className="companies-page">
-                {companies.map((company) => 
-                    <CompanyCard key={company.id} company={company} viewCampgrounds={viewCampgrounds} viewToDos={viewToDos} />
-                )}
-                </div>     */}
-        </div>
-    )
+const Company = ({ name, image_url, average_score, ...props }) => {
+  return (
+    <>
+      <div>
+        <img src={image_url} alt={name} width="50"/>
+      </div>
+      <div>
+        {name}
+      </div>
+      <div>
+        <Link to="companies" >View Airline</Link>
+      </div>
+    </>
+  )
 }
 
-export default Company;
+export default Company
